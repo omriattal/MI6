7f7f4c8f8cf910943537a458c30e9da2b1699c32
Roee Weiss Lipshitz <roeeweisslip@gmail.com>
2019-12-17 21:53:46 +0200

    New: -implemented squad NOT THREAD SAFE!!!

--- src/main/java/bgu/spl/mics/application/passiveObjects/Squad.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Squad.java
@@ -1,70 +1,107 @@
 package bgu.spl.mics.application.passiveObjects;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Passive data-object representing a information about an agent in MI6.
- * You must not alter any of the given public methods of this class. 
+ * You must not alter any of the given public methods of this class.
  * <p>
  * You may add ONLY private fields and methods to this class.
  */
-public class  Squad {
+public class Squad {
+    private Map<String, Agent> agents;
 
-	private Map<String, Agent> agents;
-	private Squad() {
-	}
-	/**
-	 * Retrieves the single instance of this class.
-	 */
-	public static Squad getInstance() {
-		//TODO: Implement this
-		return null;
-	}
+    private Squad() {
+        agents = new HashMap<>();
+    }
 
-	/**
-	 * Initializes the squad. This method adds all the agents to the squad.
-	 * <p>
-	 * @param agents 	Data structure containing all data necessary for initialization
-	 * 						of the squad.
-	 */
-	public void load (Agent[] agents) {
-		// TODO Implement this
-	}
+    /**
+     * Retrieves the single instance of this class.
+     */
+    public static Squad getInstance() {
+        return Instance.instance;
+    }
 
-	/**
-	 * Releases agents.
-	 */
-	public void releaseAgents(List<String> serials){
-		// TODO Implement this
-	}
+    /**
+     * Initializes the squad. This method adds all the agents to the squad.
+     * <p>
+     *
+     * @param agents Data structure containing all data necessary for initialization
+     *               of the squad.
+     */
+    public void load(Agent[] agents) {
+        if (agents == null) {
+            return;
+        }
+        for (Agent agent : agents) {
+            if (agent == null) {
+                this.agents.clear();
+                return;
+            }
+            this.agents.putIfAbsent(agent.getSerialNumber(), agent);
+        }
+    }
 
-	/**
-	 * simulates executing a mission by calling sleep.
-	 * @param time   milliseconds to sleep
-	 */
-	public void sendAgents(List<String> serials, int time){
-		// TODO Implement this
-	}
+    /**
+     * Releases agents.
+     */
+    public void releaseAgents(List<String> serials) {
+        for (String serial : serials) {
+            Agent agent = agents.get(serial);
+            if (agent != null) agent.release();
+        }
+    }
 
-	/**
-	 * acquires an agent, i.e. holds the agent until the caller is done with it
-	 * @param serials   the serial numbers of the agents
-	 * @return ‘false’ if an agent of serialNumber ‘serial’ is missing, and ‘true’ otherwise
-	 */
-	public boolean getAgents(List<String> serials){
-		// TODO Implement this
-		return false;
-	}
+    /**
+     * simulates executing a mission by calling sleep.
+     *
+     * @param time time ticks to sleep
+     */
+    public void sendAgents(List<String> serials, int time) throws InterruptedException {
+        Thread.sleep(time * 100);
+        releaseAgents(serials);
+    }
+
+    /**
+     * acquires an agent, i.e. holds the agent until the caller is done with it
+     * <p>
+     *
+     * @param serials the serial numbers of the agents
+     * @return ‘false’ if an agent of serialNumber ‘serial’ is missing, and ‘true’ otherwise
+     */
+    public boolean getAgents(List<String> serials) {
+        for (String serial : serials) {
+            Agent agent = agents.get(serial);
+            if (agent == null) {
+                releaseAgents(serials);
+                return false;
+            }
+            else {
+                agent.acquire();
+            }
+        }
+        return true;
+    }
 
     /**
      * gets the agents names
+     *
      * @param serials the serial numbers of the agents
      * @return a list of the names of the agents with the specified serials.
      */
-    public List<String> getAgentsNames(List<String> serials){
-        // TODO Implement this
-	    return null;
+    public List<String> getAgentsNames(List<String> serials) {
+        List<String> names = new ArrayList<>();
+        for (String serial : serials) {
+            names.add(agents.get(serial).getName());
+        }
+        return names;
+    }
+
+    private static class Instance {
+        private static Squad instance = new Squad();
     }
 
 }
