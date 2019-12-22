832ecff34e741d16975596b04e8b7f577166fe2d
Roee Weiss Lipshitz <roeeweisslip@gmail.com>
2019-12-22 03:31:46 +0200

    New: -Optional way to do the parsing, instead of doing it in main we do it in a different object and get the results from it

--- src/main/java/bgu/spl/mics/application/MI6Runner.java
+++ src/main/java/bgu/spl/mics/application/MI6Runner.java
@@ -25,13 +25,18 @@ import java.util.List;
  */
 public class MI6Runner {
     public static void main(String[] args) {
-        String filePath = args[0];
-        JsonObject jsonObject;
+        // String filePath = args[0];
+        // JsonObject jsonObject;
         try {
-            jsonObject = JsonParser.parseReader(new FileReader(filePath)).getAsJsonObject();
-            loadInventory(jsonObject);
-            loadSquad(jsonObject);
-            List<Subscriber> subscribers = loadSubscribers(jsonObject);
+            // jsonObject = JsonParser.parseReader(new FileReader(filePath)).getAsJsonObject();
+            // loadInventory(jsonObject);
+            // loadSquad(jsonObject);
+            // List<Subscriber> subscribers = loadSubscribers(jsonObject);
+
+            AppInputJsonParser inputParser = new AppInputJsonParser(args[0]);
+            Squad.getInstance().load(inputParser.getAgents());
+            Inventory.getInstance().load(inputParser.getGadgets());
+            List<Subscriber> subscribers = inputParser.getSubscribers();
             List<Thread> threadsList = new ArrayList<>();
 
             Thread newThread;
@@ -42,7 +47,8 @@ public class MI6Runner {
             }
             Thread.sleep(100);
 
-            TimeService timeService = createTimeService(jsonObject);
+            // TimeService timeService = createTimeService(jsonObject);
+            TimeService timeService = inputParser.getTimeService();
             Thread timeServiceThread = new Thread(timeService);
             timeServiceThread.start();
 
@@ -50,7 +56,7 @@ public class MI6Runner {
 
             Inventory.getInstance().printToFile(args[1]);
             Diary.getInstance().printToFile(args[2]);
-        } catch (FileNotFoundException | InterruptedException ignored) {
+        } catch (InterruptedException ignored) {
         }
     }
 
@@ -60,121 +66,108 @@ public class MI6Runner {
         }
     }
 
-    private static void confirmThreadsStarted(List<Thread> threadList) {
-        boolean threadsInitialized = false;
-        while (!threadsInitialized) {
-            threadsInitialized = true;
-            for (Thread thread : threadList) {
-                if (thread.getState() != Thread.State.WAITING) {
-                    threadsInitialized = false;
-                    break;
-                }
-            }
-        }
-    }
-
-    private static void loadInventory(JsonObject jsonObject) {
-        JsonArray inventory = jsonObject.getAsJsonArray("inventory");
-        String[] gadgets = new String[inventory.size()];
-        int index = 0;
-        for (JsonElement gadget : inventory) {
-            String nameOfGadget = gadget.getAsString();
-            gadgets[index] = nameOfGadget;
-            index++;
-        }
-        Inventory.getInstance().load(gadgets);
-    }
-
-    private static void loadSquad(JsonObject jsonObject) {
-        JsonArray squad = jsonObject.getAsJsonArray("squad");
-        Agent[] agents = new Agent[squad.size()];
-        Agent agent;
-        int index = 0;
-        for (JsonElement agentElement : squad) {
-            agent = new Agent();
-            agent.setName(agentElement.getAsJsonObject().get("name").getAsString());
-            agent.setSerialNumber(agentElement.getAsJsonObject().get("serialNumber").getAsString());
-            agent.release();
-            agents[index] = agent;
-            index++;
-        }
-        Squad.getInstance().load(agents);
-    }
-
-    private static List<Subscriber> loadSubscribers(JsonObject jsonObject) {
-        JsonObject services = jsonObject.getAsJsonObject("services");
-        int amountOfM = services.get("M").getAsInt();
-        JsonArray intelligences = services.get("intelligence").getAsJsonArray();
-        int amountOfMoneyPenny = services.get("Moneypenny").getAsInt();
-        List<Subscriber> subscribers = new ArrayList<>();
-
-        loadMoneypennySubs(amountOfMoneyPenny, subscribers);
-
-        loadMSubs(amountOfM, subscribers);
-
-        loadIntelligenceSubs(intelligences, subscribers);
-
-        subscribers.add(new Q());
-        return subscribers;
-    }
-
-    private static void loadIntelligenceSubs(JsonArray intelligences, List<Subscriber> subscribers) {
-        List<MissionInfo> missionInfoList;
-        MissionInfo missionInfo;
-        JsonArray missions;
-        JsonObject mission;
-        List<String> agentsSerials;
-        int intelId = 0;
-        for (JsonElement intelligence : intelligences) {
-            missionInfoList = new ArrayList<>();
-            missions = intelligence.getAsJsonObject().get("missions").getAsJsonArray();
-            populateMissionInfoList(missionInfoList, missions);
-            subscribers.add(new Intelligence(intelId, missionInfoList));
-            intelId++;
-        }
-    }
-
-    static void populateMissionInfoList(List<MissionInfo> missionInfoList, JsonArray missions) {
-        MissionInfo missionInfo;
-        JsonObject mission;
-        List<String> agentsSerials;
-        for (int i = 0; i < missions.size(); i++) {
-            missionInfo = new MissionInfo();
-            mission = missions.get(i).getAsJsonObject();
-            agentsSerials = new ArrayList<>();
-            populateAgentsSerials(mission, agentsSerials);
-            missionInfo.setSerialAgentsNumbers(agentsSerials);
-            missionInfo.setDuration(mission.get("duration").getAsInt());
-            missionInfo.setGadget(mission.get("gadget").getAsString());
-            missionInfo.setMissionName(mission.get("name").getAsString());
-            missionInfo.setTimeExpired(mission.get("timeExpired").getAsInt());
-            missionInfo.setTimeIssued(mission.get("timeIssued").getAsInt());
-            missionInfoList.add(missionInfo);
-        }
-    }
-
-    private static void populateAgentsSerials(JsonObject mission, List<String> agentsSerials) {
-        for (JsonElement agentSerialsElement : mission.get("serialAgentsNumbers").getAsJsonArray()) {
-            agentsSerials.add(agentSerialsElement.getAsString());
-        }
-    }
-
-    private static void loadMSubs(int amountOfM, List<Subscriber> subscribers) {
-        for (int i = 0; i < amountOfM; i++) {
-            subscribers.add(new M(i));
-        }
-    }
-
-    private static void loadMoneypennySubs(int amountOfMoneyPenny, List<Subscriber> subscribers) {
-        for (int i = 0; i < amountOfMoneyPenny; i++) {
-            subscribers.add(new Moneypenny(i));
-        }
-    }
-
-    private static TimeService createTimeService(JsonObject jsonObject) {
-        JsonObject services = jsonObject.getAsJsonObject("services");
-        int time = services.get("time").getAsInt();
-        return new TimeService(time);
-    }
+    // private static void loadInventory(JsonObject jsonObject) {
+    //     JsonArray inventory = jsonObject.getAsJsonArray("inventory");
+    //     String[] gadgets = new String[inventory.size()];
+    //     int index = 0;
+    //     for (JsonElement gadget : inventory) {
+    //         String nameOfGadget = gadget.getAsString();
+    //         gadgets[index] = nameOfGadget;
+    //         index++;
+    //     }
+    //     Inventory.getInstance().load(gadgets);
+    // }
+    //
+    // private static void loadSquad(JsonObject jsonObject) {
+    //     JsonArray squad = jsonObject.getAsJsonArray("squad");
+    //     Agent[] agents = new Agent[squad.size()];
+    //     Agent agent;
+    //     int index = 0;
+    //     for (JsonElement agentElement : squad) {
+    //         agent = new Agent();
+    //         agent.setName(agentElement.getAsJsonObject().get("name").getAsString());
+    //         agent.setSerialNumber(agentElement.getAsJsonObject().get("serialNumber").getAsString());
+    //         agent.release();
+    //         agents[index] = agent;
+    //         index++;
+    //     }
+    //     Squad.getInstance().load(agents);
+    // }
+    //
+    // private static List<Subscriber> loadSubscribers(JsonObject jsonObject) {
+    //     JsonObject services = jsonObject.getAsJsonObject("services");
+    //     int amountOfM = services.get("M").getAsInt();
+    //     JsonArray intelligences = services.get("intelligence").getAsJsonArray();
+    //     int amountOfMoneyPenny = services.get("Moneypenny").getAsInt();
+    //     List<Subscriber> subscribers = new ArrayList<>();
+    //
+    //     loadMoneypennySubs(amountOfMoneyPenny, subscribers);
+    //
+    //     loadMSubs(amountOfM, subscribers);
+    //
+    //     loadIntelligenceSubs(intelligences, subscribers);
+    //
+    //     subscribers.add(new Q());
+    //     return subscribers;
+    // }
+    //
+    // private static void loadIntelligenceSubs(JsonArray intelligences, List<Subscriber> subscribers) {
+    //     List<MissionInfo> missionInfoList;
+    //     MissionInfo missionInfo;
+    //     JsonArray missions;
+    //     JsonObject mission;
+    //     List<String> agentsSerials;
+    //     int intelId = 0;
+    //     for (JsonElement intelligence : intelligences) {
+    //         missionInfoList = new ArrayList<>();
+    //         missions = intelligence.getAsJsonObject().get("missions").getAsJsonArray();
+    //         populateMissionInfoList(missionInfoList, missions);
+    //         subscribers.add(new Intelligence(intelId, missionInfoList));
+    //         intelId++;
+    //     }
+    // }
+    //
+    // static void populateMissionInfoList(List<MissionInfo> missionInfoList, JsonArray missions) {
+    //     MissionInfo missionInfo;
+    //     JsonObject mission;
+    //     List<String> agentsSerials;
+    //     for (int i = 0; i < missions.size(); i++) {
+    //         missionInfo = new MissionInfo();
+    //         mission = missions.get(i).getAsJsonObject();
+    //         agentsSerials = new ArrayList<>();
+    //         populateAgentsSerials(mission, agentsSerials);
+    //         missionInfo.setSerialAgentsNumbers(agentsSerials);
+    //         missionInfo.setDuration(mission.get("duration").getAsInt());
+    //         missionInfo.setGadget(mission.get("gadget").getAsString());
+    //         missionInfo.setMissionName(mission.get("name").getAsString());
+    //         missionInfo.setTimeExpired(mission.get("timeExpired").getAsInt());
+    //         missionInfo.setTimeIssued(mission.get("timeIssued").getAsInt());
+    //         missionInfoList.add(missionInfo);
+    //     }
+    // }
+    //
+    // private static void populateAgentsSerials(JsonObject mission, List<String> agentsSerials) {
+    //     for (JsonElement agentSerialsElement : mission.get("serialAgentsNumbers").getAsJsonArray()) {
+    //         agentsSerials.add(agentSerialsElement.getAsString());
+    //     }
+    // }
+    //
+    // private static void loadMSubs(int amountOfM, List<Subscriber> subscribers) {
+    //     for (int i = 0; i < amountOfM; i++) {
+    //         subscribers.add(new M(i));
+    //     }
+    // }
+    //
+    // private static void loadMoneypennySubs(int amountOfMoneyPenny, List<Subscriber> subscribers) {
+    //     for (int i = 0; i < amountOfMoneyPenny; i++) {
+    //         subscribers.add(new Moneypenny(i));
+    //     }
+    // }
+    //
+    // private static TimeService createTimeService(JsonObject jsonObject) {
+    //     JsonObject services = jsonObject.getAsJsonObject("services");
+    //     int time = services.get("time").getAsInt();
+    //     return new TimeService(time);
+    // }
 
 }
